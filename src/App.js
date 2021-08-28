29da122f375db103959300f477d714d03e3058d1
Justin Fulton <jusfulton@gmail.com>
2021-08-27 20:45:09 -0700

    hook built

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import { useHTTP } from "./hooks/useHTTP";
+
+function App() {
+	const [tasks, setTasks] = useState([]);
+
+	const transformTasks = (tasksObj) => {
+		const loadedTasks = [];
+
+		for (const taskKey in tasksObj) {
+			loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+		}
+		setTasks(loadedTasks);
+	};
+
+	const {
+		isLoading,
+		error,
+		sendRequest: fetchTasks,
+	} = useHTTP(
+		{
+			url: "https://tasklist-tester-default-rtdb.firebaseio.com/tasks.json",
+		},
+		transformTasks,
+	);
+
+	useEffect(() => {
+		fetchTasks();
+	}, []);
+
+	const taskAddHandler = (task) => {
+		setTasks((prevTasks) => prevTasks.concat(task));
+	};
+
+	return (
+		<React.Fragment>
+			<NewTask onAddTask={taskAddHandler} />
+			<Tasks
+				items={tasks}
+				loading={isLoading}
+				error={error}
+				onFetch={fetchTasks}
+			/>
+		</React.Fragment>
+	);
+}
+
+export default App;
